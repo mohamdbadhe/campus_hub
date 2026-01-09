40928123987b32dd018cd3ec6baac2e4688dc459
zainaas-wq <zainaas@ac.sce.ac.il>
2026-01-09 18:55:03 +0200

    US-9: implement login/logout with role-based access

--- crud/settings.py
+++ crud/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django_extensions',
     'accounts',
 ]
 
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'crud.urls'
